e331959feef104b41d45b8affeb06c56e4d1509c
Vinicius Martins <vinicius_marp500@hotmail.com>
2022-05-16 20:39:37 -0300

    Category

---
+++ recipes/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from utils.recipes.factory import make_recipe
+
+from recipes.models import Recipe
+
+
+def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id = category_id)
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
