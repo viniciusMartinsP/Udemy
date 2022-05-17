1316e298e6600a281e2d5371d0f1de435d5ab9a2
Vinicius Martins <vinicius_marp500@hotmail.com>
2022-05-17 09:08:50 -0300

    category

--- recipes/views.py
+++ recipes/views.py
@@ -5,14 +5,14 @@ from recipes.models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.all().order_by('-id').filter(is_published=True)
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
-    })
+    }) 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id = category_id)
-    return render(request, 'recipes/pages/home.html', context={
+    recipes = Recipe.objects.filter(category__id = category_id, is_published=True)
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
     })
 
