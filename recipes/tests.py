c22fdbf4b4289771cb9179cef799e6ffac273875
Vinicius Martins <vinicius_marp500@hotmail.com>
2022-05-18 16:18:22 -0300

    test recipe category

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,17 @@
+from audioop import reverse
+from turtle import home
+
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+class RecipeURLsTest(TestCase):
+    def test_recipe_home_url_is_correct(self):
+        url = reverse('recipes:home')
+        self.assertEqual(url, '/')
+    
+    def test_recipe_category_url_is_ok(self):
+        url = reverse('recipes:category', kwargs={'category_id':1})
+        self.assertEqual(url, '/recipes/category/1/')
+
