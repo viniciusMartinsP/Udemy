c412ebeaff0e906d0b27b3eb93269ccb39ef7208
Vinicius Martins <vinicius_marp500@hotmail.com>
2022-05-18 10:23:12 -0300

    empty recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,24 +1,34 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from utils.recipes.factory import make_recipe
 
 from recipes.models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id').filter(is_published=True)
+    recipes = Recipe.objects.filter(
+        is_published=True,
+    ).order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
-    }) 
+        'recipes':recipes,
+    })
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id = category_id, is_published=True)
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
+    
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
     })
 
-
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
