50ca8123761ae014c56f69adfe821d5ee31990b0
Vinicius Martins <vinicius_marp500@hotmail.com>
2022-05-23 09:38:31 -0300

    querys working

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,8 +5,8 @@ from recipes import views
 app_name = 'recipes'
 
 urlpatterns = [
+    path('recipes/search/', views.search, name="search"),
     path('', views.home, name="home"),
     path('recipes/category/<int:category_id>/', views.category, name="category"),
     path('recipes/<int:id>/', views.recipe, name="recipe"),
-    path('recipes/search/', views.search, name="search"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 from utils.recipes.factory import make_recipe
@@ -34,4 +35,28 @@ def recipe(request, id):
     })
 
 def search(request):
-    return render(request,'recipes/pages/search.html') 
+    search_term = request.GET.get('search','').strip()
+
+    if not search_term:
+        raise Http404()
+
+
+
+    '''O Q('something')|Q(other thing) faz com que o django procure por um ou outro
+        o __contains pesquisa como se fosse %algumacoisa%.. já o __icontains pesquisa
+        ignorando case sensitive '''
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains = search_term) | 
+            Q(description__icontains=search_term),
+        ), 
+        is_published=True
+    ).order_by('-id')
+
+    return render(request,'recipes/pages/search.html', {
+        'page_title': f'Search for {search_term}|',
+        'search_term': search_term,
+        'recipes': recipes,
+    })
+
+
