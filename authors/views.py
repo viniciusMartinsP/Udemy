4e509f79116605c5d091f699121ff16a7da2f0fa
Vinicius Martins <vinicius_marp500@hotmail.com>
2022-06-10 17:27:00 -0300

    creating dashboard authors and coverage 100%

--- authors/views.py
+++ authors/views.py
@@ -34,7 +34,6 @@ def register_create(request):
         del(request.session['register_form_data'])
         return redirect(reverse('authors:login'))
 
-
     return redirect('authors:register')
 
 
@@ -51,7 +50,6 @@ def login_create(request):
         raise Http404()
 
     form = LoginForm(request.POST)
-    login_url = reverse('authors:login')
 
     if form.is_valid():
         authenticated_user = authenticate(
@@ -63,19 +61,28 @@ def login_create(request):
             messages.success(request, 'Your are logged in.')
             login(request, authenticated_user)
         else:
-            messages.   error(request, 'Invalid credentials')
+            messages.error(request, 'Invalid credentials')
     else:
         messages.error(request, 'Invalid username or password')
 
-    return redirect(login_url)
+    return redirect(reverse('authors:dashboard'))
+
 
 @login_required(login_url='authors:login', redirect_field_name='next')
 def logout_view(request):
     if not request.POST:
+        messages.error(request, 'Invalid logout request')
         return redirect(reverse('authors:login'))
 
-    if request.POST.get('username')!= request.user.username:
+    if request.POST.get('username') != request.user.username:
+        messages.error(request, 'Invalid logout user')
         return redirect(reverse('authors:login'))
 
+    messages.success(request, 'Logged out successfuly')
     logout(request)
     return redirect(reverse('authors:login'))
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard(request):
+    return render(request, 'authors/pages/dashboard.html')
--- tests/functional_tests/authors/test_authors_login_form.py
+++ tests/functional_tests/authors/test_authors_login_form.py
@@ -1,4 +1,5 @@
-from pydoc import resolve
+
+from time import sleep, time
 
 import pytest
 from django.contrib.auth.models import User
@@ -14,7 +15,7 @@ class AuthorsLoginTest(AuthorsBaseTest):
         string_password = 'pass'
         user = User.objects.create_user(username='my_user', password='pass')
 
-        self.browser.get(self.live_server_url + reverse('authors:login'))
+        self.browser.get(self.live_server_url+reverse('authors:login'))
 
         form = self.browser.find_element(By.CLASS_NAME, 'main-form')
         username_field = self.get_by_placeholder(form, 'Type your username')
@@ -24,9 +25,46 @@ class AuthorsLoginTest(AuthorsBaseTest):
         password_field.send_keys(string_password)
 
         form.submit()
+        sleep(3)
         self.assertIn(
             f'You are logged in with {user.username}',
-            self.browser.find_element(By.TAG_NAME,'body').text
+            self.browser.find_element(By.TAG_NAME, 'body').text
         )
-        
 
+    def test_login_create_raises_404_if_not_POST_method(self):
+        self.browser.get(
+            self.live_server_url +
+            reverse('authors:login_create')
+        )
+        self.assertIn(
+            'The requested resource was not found on this server',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
+
+    def test_form_login_is_invalid_message(self):
+        self.browser.get(self.live_server_url + reverse('authors:login'))
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+        username_field = self.get_by_placeholder(form, 'Type your username')
+        password_field = self.get_by_placeholder(form, 'Type your password')
+
+        username_field.send_keys('')
+        password_field.send_keys('')
+        form.submit()
+        self.assertIn(
+            'Invalid username or password',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
+
+    def test_trying_input_invalid_credentials_error(self):
+        self.browser.get(self.live_server_url + reverse('authors:login'))
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+        username_field = self.get_by_placeholder(form, 'Type your username')
+        password_field = self.get_by_placeholder(form, 'Type your password')
+
+        username_field.send_keys('somethingStrange')
+        password_field.send_keys('somethingStrangeMORE$trange')
+        form.submit()
+        self.assertIn(
+            'Invalid credentials',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
